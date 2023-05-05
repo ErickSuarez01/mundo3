d24eadc6806105a06f4799da3dcc89186f4fa7dd
Erick Suarez <erickroberto.suarez@viewnext.com>
2023-05-05 13:37:07 +0200

    mundo3

---
+++ src/test/java/demo/AppTest.java
@@ -0,0 +1,47 @@
+package demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void mostrarDebug(){
+    	log debug= new log();
+    	debug.debug();
+    	assertTrue(true);
+    }
+    @Test
+    public void mostrarInfo(){
+    	log debug= new log();
+    	debug.info();
+    	assertTrue(true);
+    }
+    @Test
+    public void mostrarWrning(){
+    	log debug= new log();
+    	debug.warning();
+    	assertTrue(true);
+    }
+    @Test 
+    public void main(){
+        App.main(new String[]{});
+    }
+     @Test 
+    public void main2(){
+        log.main(new String[]{});
+    }
+}
